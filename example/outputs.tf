cc45740c769ede519aa2de72d4fe4b9a185361fb
jijun.jj <jijun.jj@alibaba-inc.com>
2019-04-15 18:45:32 +0800

    1.modify the name of output paramters

--- example/outputs.tf
+++ example/outputs.tf
@@ -1,19 +1,19 @@
 output "vpn_id" {
-  value = "${module.vpn.this_vpn_id}"
+  value = "${module.vpn.this_vpn_gateway_id}"
 }
 
 output "cgw_id" {
-  value = "${module.vpn.this_cgw_id}"
+  value = "${module.vpn.this_vpn_customer_gateway_id}"
 }
 
 output "vss_id" {
-  value = "${module.vpn.this_vss_id}"
+  value = "${module.vpn.this_ssl_vpn_server_id}"
 }
 
 output "vsc_ids" {
-  value = "${module.vpn.this_vsc_ids}"
+  value = "${module.vpn.this_ssl_vpn_client_cert_ids}"
 }
 
 output "vco_id" {
-  value = "${module.vpn.this_vco_id}"
+  value = "${module.vpn.this_vpn_connection_id}"
 }
