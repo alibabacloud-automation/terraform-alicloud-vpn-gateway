faea6414e081e328ea70db5e901f1815fce354a6
jijun.jj <jijun.jj@alibaba-inc.com>
2019-04-15 18:20:14 +0800

    1.Complete README 2.Add Prefix of output paramters

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,24 @@
-output "vpn_id" {
+output "this_vpn_id" {
   description = "The ID of VPN gateway instance."
   value       = "${alicloud_vpn_gateway.vpngw.id}"
 }
 
-output "cgw_id" {
+output "this_cgw_id" {
   description = "The ID of customer gateway instance."
   value       = "${join(",", alicloud_vpn_customer_gateway.vpncgw.*.id)}"
 }
 
-output "vss_id" {
+output "this_vss_id" {
   description = "The ID of SSL-VPN server instance."
   value       = "${join(",", alicloud_ssl_vpn_server.sslserver.*.id)}"
 }
 
-output "vsc_ids" {
+output "this_vsc_ids" {
   description = "The IDs of SSL-VPN client certs."
   value       = "${join(",", alicloud_ssl_vpn_client_cert.certs.*.id)}"
 }
 
-output "vco_id" {
+output "this_vco_id" {
   description = "The ID of VPN connection."
   value       = "${join(",", alicloud_vpn_connection.connection.*.id)}"
 }
