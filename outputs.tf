b27bea5dbc4ca4986b81d7b9f3fea48f300fc402
lexwong <lexwong@lexwongdeMacBook-Air.local>
2019-09-24 18:55:52 +0800

    imporve(vpn gateway): update the module to the format of the new version.

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,25 @@
 output "this_vpn_gateway_id" {
   description = "The ID of VPN gateway instance."
-  value       = "${alicloud_vpn_gateway.vpngw.id}"
+  value       = alicloud_vpn_gateway.vpngw.id
 }
 
 output "this_vpn_customer_gateway_id" {
   description = "The ID of customer gateway instance."
-  value       = "${join(",", alicloud_vpn_customer_gateway.vpncgw.*.id)}"
+  value       = join(",", alicloud_vpn_customer_gateway.vpncgw.*.id)
 }
 
 output "this_ssl_vpn_server_id" {
   description = "The ID of SSL-VPN server instance."
-  value       = "${join(",", alicloud_ssl_vpn_server.sslserver.*.id)}"
+  value       = join(",", alicloud_ssl_vpn_server.sslserver.*.id)
 }
 
 output "this_ssl_vpn_client_cert_ids" {
   description = "The IDs of SSL-VPN client certs."
-  value       = "${join(",", alicloud_ssl_vpn_client_cert.certs.*.id)}"
+  value       = join(",", alicloud_ssl_vpn_client_cert.certs.*.id)
 }
 
 output "this_vpn_connection_id" {
   description = "The ID of VPN connection."
-  value       = "${join(",", alicloud_vpn_connection.connection.*.id)}"
+  value       = join(",", alicloud_vpn_connection.connection.*.id)
 }
+
